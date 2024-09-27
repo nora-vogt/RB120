80c9a64dbe5215692d0bf7fd2d95ada81d24220c
Nora Vogt <noravogt@protonmail.com>
2024-09-27 19:12:35 -0400

    added tracking of round winner to RPSGame

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,4 +1,19 @@
-# Refactoring in OO RPS Design Choice 2
+=begin
+# Implementing score as a state of the Player class (that Human and Computer) will inherit.
+
+Player
+  - has a @score attribute, initialize to 0
+    - can make the reader public
+
+RPSGame
+  - needs a @round_winner
+    - maybe a determine_round_winner that also updates score?
+    - then display_score uses @round_winner
+  - needs a @game_winner
+  - update_score
+  - reset_score (when game is won / before starting a new game)
+=end
+
 class Move
   VALUES = ['rock', 'paper', 'scissors']
 
@@ -36,10 +51,11 @@ class Move
 end
 
 class Player
-  attr_accessor :move, :name
+  attr_accessor :move, :name, :score
 
   def initialize
     set_name
+    @score = 0
   end
 end
 
@@ -79,11 +95,12 @@ end
 
 # Game Orchestration Engine
 class RPSGame
-  attr_accessor :human, :computer
+  attr_accessor :human, :computer, :round_winner
 
   def initialize
     @human = Human.new
     @computer = Computer.new
+    @round_winner = nil
   end
 
   def display_welcome_message
@@ -99,14 +116,24 @@ class RPSGame
     puts "#{computer.name} chose #{computer.move}."
   end
 
-  def display_winner
+  def determine_round_winner
     if human.move > computer.move
-      puts "#{human.name} won!"
+      self.round_winner = human
     elsif human.move < computer.move
-      puts "#{computer.name} won!"
+      self.round_winner = computer
+    end
+  end
+
+  def display_round_winner
+    if round_winner
+      puts "#{round_winner.name} won!"
     else
       puts "It's a tie!"
     end
+  end
+
+  def reset_round_winner
+    self.round_winner = nil
   end
 
   def play_again?
@@ -128,8 +155,12 @@ class RPSGame
       human.choose
       computer.choose
       display_moves
-      display_winner
+      determine_round_winner
+      display_round_winner
+      #update_score
+      #display_score
       break unless play_again?
+      reset_round_winner
     end
 
     display_goodbye_message
